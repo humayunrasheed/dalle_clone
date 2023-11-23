4dc0b3500b10aae4a593aab025bc9fdf075f2cb1
humayunrasheed <104017282+humayunrasheed@users.noreply.github.com>
2023-11-24 00:28:37 +0530

    CreatePost compleated

---
+++ server/mongodb/connect.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const connectDB = (url) => {
+  mongoose.set('strictQuery', true);
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch((error) => console.log(error.message));
+};
+
+export  default connectDB ;
