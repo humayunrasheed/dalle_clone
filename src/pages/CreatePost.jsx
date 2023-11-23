dcdb78395104ca658da1a8329b746e0fa94b2b50
humayunrasheed <104017282+humayunrasheed@users.noreply.github.com>
2023-11-24 00:29:35 +0530

    Createds post compleated

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import {getRandomPrompt} from '../utils'
 import { FormField,Loader } from '../components'
 import assets from '../assets';
+import { fromHalfFloat } from 'three/src/extras/DataUtils'
 const { preview } = assets;
 
 
@@ -16,11 +17,51 @@ const CreatePost = () => {
   });
   const [loading, setLoading] = useState(false);
   const [generatingImg, setGeneratingImg] = useState(false);
-  const generateImage =  () => {
+  const generateImage =  async() => {
+    if(form.prompt){
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ prompt: form.prompt })
+        });
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.image}` });
+      } catch (error) {
+        alert(error);
+      }finally{
+        setGeneratingImg(false);
+      }
+    }else{
+      alert('Please enter a prompt');
+    }
   }
   
-  const handleSubmit = () => {
-  
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    if(form.prompt && form.photo){
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/posts', {  
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(form)
+        });
+        await response.json();
+        navigate('/');
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    }else{
+      alert('Please enter a prompt and generate an image');
+    }
   }
   
   const handleChange = (e) => {
