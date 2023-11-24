bedf59888f3df668544a3d3be38d2c59fc2fdfbe
humayunrasheed <104017282+humayunrasheed@users.noreply.github.com>
2023-11-24 11:55:14 +0530

    push

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -6,10 +6,10 @@ dotenv.config();
 
 const router = express.Router();
 
-
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
-});  
+});
+
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: 'Hello from DALL-E!' });
@@ -19,18 +19,18 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const aiResponse = await openai.createImage({
-      prompt,
+    const aiResponse = await openai.createImages({
+      prompt:prompt,
       n: 1,
       size: '1024x1024',
       response_format: 'b64_json',
     });
-
     const image = aiResponse.data.data[0].b64_json;
+    console.log(aiResponse.data)
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
-    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+    res.status(500).send('Something went wrong');
   }
 });
 
