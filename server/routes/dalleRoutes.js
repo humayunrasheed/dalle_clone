b93274f10b1775349c3a706856644d5e2fc774fb
humayunrasheed <104017282+humayunrasheed@users.noreply.github.com>
2023-11-24 10:00:39 +0530

    Cards section done

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -19,14 +19,14 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const aiResponse = await openai.images.generate({
-      prompt,
+    const aiResponse = await openai.createImage({
+      prompt:prompt,
       n: 1,
       size: '1024x1024',
       response_format: 'b64_json',
     });
-
     const image = aiResponse.data.data[0].b64_json;
+    console.log(aiResponse.data)
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
